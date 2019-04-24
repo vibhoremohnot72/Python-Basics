cb951053b5790325082ba9127ecaca6a5e50c4c3
Vibhore Mohnot <45279814+vibhoremohnot72@users.noreply.github.com>
2019-04-24 18:11:34 +0530

    modificationindistance

--- distanceformula.py
+++ distanceformula.py
@@ -1,7 +1,9 @@
 import math
 x1 = float(input("Enter x-coordinate of first point: "))
 y1 = float(input("Enter y-coordinate of first point: "))
+z1 = float(input("Enter z-coordinate of first point: "))
 x2 = float(input("Enter x-coordinate of second point: "))
 y2 = float(input("Enter y-coordinate of second point: "))
-distance = math.sqrt(((x2 - x1)**2) + ((y2 - y1)**2))
+z2 = float(input("Enter z-coordinate of second point: "))
+distance = math.sqrt(((x2 - x1)**2) + ((y2 - y1)**2) + ((z2 - z1)**2))
 print (distance)
