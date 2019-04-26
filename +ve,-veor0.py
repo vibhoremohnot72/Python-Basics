2e37df4a952ccdbd6a50eca337ae0e3f609716ce
Vibhore Mohnot <45279814+vibhoremohnot72@users.noreply.github.com>
2019-04-26 12:35:08 +0530

    commit

---
+++ +ve,-veor0.py
@@ -0,0 +1,7 @@
+x = float(input("Enter a number: "))
+if x == 0:
+    print ("Value is zero.")
+elif x > 0:
+    print ("Value is positive.")
+else:
+    print ("Value is negative.")
---
+++ swapping two values.py
@@ -0,0 +1,5 @@
+a = input("Enter a value: ")
+b = input("Enter another value: ")
+print ("Before swapping a = %s and b = %s" % (a,b))
+a, b = b, a
+print ("After swapping a = %s and b = %s" % (a,b))
